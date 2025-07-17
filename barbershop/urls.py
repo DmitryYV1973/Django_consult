fbbbf7159b0a6e26e7ae0adcbaf20a8bfab7bc44
Dmitry Yudin <yudin.dm2013@mail.ru>
2025-07-17 03:45:58 +0300

    feat: конвертер путей int и страничка masters_detail

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import main
+from core.views import main, masters_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main),
+    path('masters/<int:master_id>/', masters_detail),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+masters = [
+    {"id": 1, "name": "Иван Петров"},
+    {"id": 2, "name": "Анна Смирнова"},
+    {"id": 3, "name": "Сергей Иванов"},
+    {"id": 4, "name": "Елена Козлова"},
+    {"id": 5, "name": "Дмитрий Соколов"}
+]
+
+
 def main(request):
     return HttpResponse("Добро пожаловать в наш барбершоп!")
+
+
+def masters_detail(request, master_id):
+    try:
+        master = [master for master in masters if master["id"] == master_id][0]
+    except IndexError:
+        return HttpResponse("Мастер не найден")
+    return HttpResponse(f"<h1>{master['name']}</h1>")
+        
+

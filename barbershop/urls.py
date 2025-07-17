e739283aa268fc84b9659d9295076738f8952306
Dmitry Yudin <yudin.dm2013@mail.ru>
2025-07-17 08:39:52 +0300

    hw_37: исправлены маршруты

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import main
 from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main),
-    path('barbershop/', include('core.urls')),
+    path('', include('core.urls')),
 ]

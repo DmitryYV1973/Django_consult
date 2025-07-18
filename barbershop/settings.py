65c9b160806fcd03992baa408388704bc8bef70c
Dmitry Yudin <yudin.dm2013@mail.ru>
2025-07-18 22:34:38 +0300

    hw_39:  сбор статики для отображения изображений и улучшения фронтенда

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # Настраиваем статику
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Для загрузки изображений
 MEDIA_URL = '/media/'

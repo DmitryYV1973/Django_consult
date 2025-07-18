7f3f2ac998d0c0dd9b15d7e2fc78046d52d94d2a
Dmitry Yudin <yudin.dm2013@mail.ru>
2025-07-18 20:06:09 +0300

    hw_39: исправлены неточности

--- core/models.py
+++ core/models.py
@@ -1,25 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class Master(models.Model):
-    name = models.CharField(max_length=150, verbose_name="Имя")
-    photo = models.ImageField(upload_to="masters/", blank=True, verbose_name="Фотография")
-    phone = models.CharField(max_length=20, verbose_name="Телефон")
-    address = models.CharField(max_length=255, verbose_name="Адрес")
-    experience = models.PositiveIntegerField(
-        verbose_name="Стаж работы", 
-        help_text="Опыт работы в годах"
-    )
-    is_active = models.BooleanField(default=True, verbose_name="Активен")
-
-    class Meta:
-        verbose_name = "Мастер"
-        verbose_name_plural = "Мастера"
-        ordering = ['-is_active', 'name']
-
-    def __str__(self):
-        return f"{self.name} (стаж: {self.experience} лет)"
-
 class Service(models.Model):
     name = models.CharField(max_length=200, verbose_name="Название")
     description = models.TextField(blank=True, verbose_name="Описание")
@@ -42,6 +23,32 @@ class Service(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.price} руб."
+
+class Master(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Имя")
+    photo = models.ImageField(upload_to="masters/", blank=True, verbose_name="Фотография")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    address = models.CharField(max_length=255, verbose_name="Адрес")
+    experience = models.PositiveIntegerField(
+        verbose_name="Стаж работы", 
+        help_text="Опыт работы в годах"
+    )
+    services = models.ManyToManyField(
+        Service,
+        related_name="masters",
+        verbose_name="Услуги"
+    )
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    class Meta:
+        verbose_name = "Мастер"
+        verbose_name_plural = "Мастера"
+        ordering = ['-is_active', 'name']
+
+    def __str__(self):
+        return f"{self.name} (стаж: {self.experience} лет)"
+
+
 
 class Order(models.Model):
     STATUS_CHOICES = [

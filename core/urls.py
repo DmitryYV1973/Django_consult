8da543bc2d4e68a43d51c094ba390eae3d4df36c
Dmitry Yudin <yudin.dm2013@mail.ru>
2025-07-22 07:25:11 +0300

    hw_41: добавление маршрута services/create/

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,15 @@
-# core/urls.py
-
 from django.urls import path
-from .views import LandingView, ThanksView, OrdersListView, OrderDetailView, service_create_view
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import LandingView, ThanksView, OrdersListView, OrderDetailView, ServiceCreateView
 
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
     path('thanks/', ThanksView.as_view(), name='thanks'),
     path('orders/', OrdersListView.as_view(), name='orders_list'),
-    path('order/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
-    path('services/create/', service_create_view, name='service_create'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
+    path('services/create/', ServiceCreateView.as_view(), name='service_create'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
